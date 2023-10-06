2538043d75936c8fdb051efcaf9212f095b2527f
Raymond <raymond.mwaiki@student.moringaschool.com>
2023-10-06 09:00:55 +0300

    Done

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
 function scuberGreetingForFeet(){
   // Write your code here!
+
+let result
+if(x<=400){
+  return 'This one is on me!';
+}
+else if(x>400 && X<=2500){
+  return 'I will gladly take your thirty bucks'
+}
+else {
+  return 'No can do';
+}
 }
 
 function ternaryCheckCity(){
   // Write your code here!
+return city ==="NYC"? "Ok, sounds good." : "No go.";
 }
+
 
 function switchOnCharmFromTip(){
   // Write your code here!
-}
+  switch (tip) {
+    case "generous":
+     return "Thank you so much"
+    break;
+    case "not as generous":
+    return "Thank you"
+    break
+    default:
+      return "Bye."
+  }
+  }
